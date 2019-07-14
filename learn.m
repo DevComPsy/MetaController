6aa11161b2d1a77f10905dc558074511677aed85
Alisa Loosen <a.loosen.17@ucl.ac.uk>
2019-07-14 22:51:53 +0100

    Update learn.m

--- learn.m
+++ learn.m
@@ -1,35 +1,18 @@
-function mc = learn(mc,mc_a,mc_s,g,alpha)
+function mc = learn(mc,nactions,alpha, nstates, niter)
 
-rew = mc.rew(g);
-% rew_b = mc.rew_b(g);
 
-for i = 1:length(mc.phi)
-    % calc PE
-    if size(mc.q_state{i},2) < mc_s     % too simplistic model
-        q = mean(mc.q_state{i}(mc_a,:));    % mean prediction
-    else
-        q = mc.q_state{i}(mc_a,mc_s);
-    end
+for i = 1:length(n_iter)
     
-    mc.pe{i}(g) = rew - q;
-    
-%     mc.pe_b{i}(g) = rew_b - q;
-%     
-    % update Q
-    if size(mc.q_state{i},2) < mc_s
-        mc.q_state{i}(mc_a,:) = mc.q_state{i}(mc_a,:) + alpha*mc.pe{i}(g);
-    else
-        mc.q_state{i}(mc_a,mc_s) = mc.q_state{i}(mc_a,mc_s) + alpha*mc.pe{i}(g);
-    end
-    
-    
-%     % update Q for b    
-%     if size(mc.q_state{i},2) < mc_s
-%         mc.q_state{i}(mc_a,:) = mc.q_state{i}(mc_a,:) + alpha*mc.pe_b{i}(g);
-%     else
-%         mc.q_state{i}(mc_a,mc_s) = mc.q_state{i}(mc_a,mc_s) + alpha*mc.pe_b{i}(g);
-%     end
-%     
-    
+ % update 
+% mc.shown_stimulus
 
+% mc.choice
+
+mc.V_s=[mean(mc.outcome|mc.choice==1); mean(mc.outcome|mc.choice==2)];
+
+mc.pe_mf= mc.outcomes - mc.Qmf
+mc.Qmf= mc.Qmf+ alpha*(mc.pe_mf);
+
+mc.pe_mb= mc.outcomes - mc.Qmb
+mc.Qmb = mc.Qmb + alpha*(mc.pe_mb);
 end
