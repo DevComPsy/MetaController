0a3d4e39410b668067474c7af922c047f40fde7b
Alisa Loosen <a.loosen.17@ucl.ac.uk>
2019-07-14 22:52:40 +0100

    Update metaCont_act.m

--- metaCont_act.m
+++ metaCont_act.m
@@ -1,12 +1,13 @@
-function [mc_a,mc_s,idx,idx_b] = metaCont_act(outcome)
+function [idx] = metaCont_act(mc,g)
 
-% select action of the system with highest conficence (argmin)
+
+% select action of the system with highest confidence (argmin) for the
+% specific states
+
+mc.shown_stimulus
+
 [~,idx] = min(mc.phi);
-
 
 tmp_a = mc.act(idx,g);
 
-
 [mc_a,mc_s] = ind2sub(size(mc.pi{idx}),tmp_a);
-
-
