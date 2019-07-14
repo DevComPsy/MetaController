90e8513ebe8e2d4c327f437392ff46f8785fe304
Alisa Loosen <a.loosen.17@ucl.ac.uk>
2019-07-14 22:53:12 +0100

    Update learn_phi.m

--- learn_phi.m
+++ learn_phi.m
@@ -1,6 +1,8 @@
-function mc = learn_phi(mc,g)
+function mc = learn_phi(mc)
 
 for i = 1:length(mc.phi)
-    mc.phi(i) = mc.phi(i) + abs(mc.pe{i}(g));
+    mc.phi_mf{i} = mc.phi(i) + abs(mc.pe_mf{i});
     
+    mc.phi_mb{i} = mc.phi(i) + abs(mc.pe_mb{i});
+
 end
