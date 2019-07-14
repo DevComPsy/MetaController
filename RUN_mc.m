23ff9bb9fdcac359ea2d5a04b64ed3856e5a57b4
Alisa Loosen <a.loosen.17@ucl.ac.uk>
2019-07-14 22:51:08 +0100

    Update RUN_mc.m

--- RUN_mc.m
+++ RUN_mc.m
@@ -1,34 +1,31 @@
 clear all; close all; clc
 
-%% settings
-a_tot = [2,2,2];%number of actions 
-s_tot = [1,2,3];%number of state dimensions
-n_iter = 20;
-alpha = .3;
+     alpha = .3
+     niter = 20;
+     nstates = 2;
+     nactions = 2;
 
 
 %% initialize
-[mc] = mc_initialize(n_iter,a_tot,s_tot);
+[mc] = mc_initialize(alpha,nstates,nactions);
 
 
 %% loop through games
-for g = 1:n_iter
-    [rew] = task_a(mc);%function simulating task a
-    
+for g = 1:niter
+    [shown_stimulus, outcomes] = task_a(mc);%function simulating task a
     
     % action selection each system
     [mc,a] = act(mc);
     mc.act(:,g) = a;
     
-    % action selection meta controller
-    [mc_a,mc_s,sys_chos] = metaCont_act(mc,g);
-    mc.rew(g) = rew{sys_chos}(mc_a,mc_s);
+    % action selection meta-controller
+    [nstates,nactions,sys_chos] = metaCont_act(mc,g);
+    mc.rew(g) = rew{sys_chos}(nstates,nactions);
     
     % PE learning
-    mc = learn(mc,mc_a,mc_s,g,alpha);
-    
+    mc = learn(mc,nactions,alpha,nstates,niter);
+
     % update confidence
     mc = learn_phi(mc,g);
     
 end
-    % plot
