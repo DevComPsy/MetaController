cd66397353d1c3d478ad2bffc822d2b947b635b8
Alisa Loosen <a.loosen.17@ucl.ac.uk>
2019-07-15 23:53:15 +0100

    Update task_a.m

--- task_a.m
+++ task_a.m
@@ -1,18 +1,11 @@
-function [mc] = task_a(mc)
+function [mc, task] = task_a(mc)
 % Implementation of a simple motor task, one action gives rewards
 
-
-mc.stimuli_sign=(randperm(2)); % 1=star, 2= circle
+task.stimuli_backgr=(randi([1 2]));
+task.stimuli_sign=(randperm(2)); % 1=star, 2= circle
 
 %% play one game
-outcome_prob= [rand(1)<=.8 rand(1)<=.2];% action a (e.g. left press) 80% reward; action b (e.g. right press) 20% reward
+task.all_outcome_prob= [rand(1)<=.8 rand(1)<=.2];% action a (e.g. left press) 80% reward; action b (e.g. right press) 20% reward
 
-mc.shown_stimulus= [mc.stimuli_sign(:,1) stimuli_sign(:,2) NaN NaN];
-
-%% select an action
-a = act(mc); %metacontroller determines the executed action given the presented stimuli
-mc.choice = metaCont_act(mc,a);
-
-mc.outcomes=[rand(1)<=outcome_prob]; 
-
+task.shown_stimulus= [1 task.stimuli_sign(1)  task.stimuli_backgr];
 end
