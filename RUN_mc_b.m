e961cc488777f0a88b1e13f4ffe44c882f718946
Alisa Loosen <a.loosen.17@ucl.ac.uk>
2019-07-15 23:48:37 +0100

    Update RUN_mc_b.m

--- RUN_mc_b.m
+++ RUN_mc_b.m
@@ -1,34 +1,37 @@
 clear all; close all; clc
-
-
-     alpha = .3
+     alpha = .3;
+     alphasystem = .3;
      niter = 20;
      nstates = 2;
      nactions = 2;
+     invtemp= 1;
+     nsystems=2;
+     cost = 1;
+     
 
 %% initialize
- [mc] = mc_initialize(alpha,nstates,nactions);
-
+[mc] = mc_initialize(alpha,nstates,nactions, nsystems);
+[task] = task_initialize(niter,nstates,nactions);
 
 %% loop through games
 for g = 1:niter
-    [chosen_stimulus,outcomes] = task_b(mc); %function simulating task b
     
-    % action selection each system  
-    [mc,a] = act(mc);
-    mc.act(:,g) = a;
+      % function simulating task a
+    [mc, task] = task_b(mc, task);
     
-    % action selection meta-controller
-    [nstates,nactions,sys_chos] = metaCont_act(mc,g);
-    mc.outcome(g) = outcome{sys_chos}( nstates,nactions);
+      % action selection each system
+    [mc, task]=act(mc, task); 
     
-    % PE learning
-    mc = learn(mc,nstates,alpha);
-  
-    
-    % update confidence
-    mc = learn_phi(mc,g);
+      % action selection meta-controller
+    [idx, mc, task] = metaCont_act(mc, task);
+
+    task.outcome_prob = task.all_outcome_prob(task.choice); 
+    task.outcome=rand(1)<=task.outcome_prob; 
+      
+     % PE learning
+    [mc] = learn(mc,task, nactions,alpha,nstates, idx, niter);
+
+     % update confidence
+    [mc] = learn_phi(mc,alphasystem,cost,idx, task, nsystems);
     
 end
-
- % plot
